a45c8b46e3ae5a2955d9eb71f685ff4dcfde5f85
Vuong Q. Huy <119883477+vnghuygithub@users.noreply.github.com>
2023-09-07 20:44:49 +0700

    feat: tag, bug: call api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import { useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
-import SearchResult from './components/SearchResults';
-import SearchResultList from './components/SearchResultList';
 
 function App() {
-
-  const [results, setResults] = useState([])
-  const [finalResults, setFinalResults] = useState([])
-  const [fieldsNames, setFieldsNames] = useState([])
-  const [searchValue, setSearchValue] = useState("");
-
-  const handleSearchValueChange = (value) => {
-    setSearchValue(value);
-  };
-
-  console.log(finalResults);
-
   return (
-    <div className='container' style={{background: "grey", height: 360}}>
-      <SearchBar setResults={setResults} setFieldsNames={setFieldsNames} onInputChange={handleSearchValueChange} />
-      <SearchResult results={results} fieldsNames={fieldsNames} searchValue={searchValue} setFinalResults={setFinalResults}/>
-      <SearchResultList finalResults={finalResults}/>
+    <div className='container' style={{ backgroundColor: 'grey', height: 500}}>
+      <SearchBar  />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,44 +1,139 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./SearchBar.css";
 
-function SearchBar({ setResults, setFieldsNames, onInputChange }) {
-  const [input, setInput] = useState("");
+function SearchBar() {
+  const [fieldNames, setFieldNames] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState({
+    fieldNames: [],
+    inputValues: [],
+  });
 
-  const fetchData = (value) => {
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+
+  const [inputValues, setInputValues] = useState([]);
+
+  const handleChange = (event) => {
+    const newInputValue = event.target.value;
+    setInputValue(newInputValue);
+    fetchData(newInputValue);
+
+    setInputValues([...inputValues, newInputValue]);
+  };
+
+  const toggleDropdown = () => {
+    const dropdown = document.getElementById("dropdownMenu");
+    if (dropdown.style.display === "block" && inputValue.trim() === "") {
+      dropdown.style.display = "none";
+    } else {
+      dropdown.style.display = "block";
+    }
+  };
+
+  const addOption = (fieldName, inputValue) => {
+    const { fieldNames, inputValues } = selectedOptions;
+    const updatedFieldNames = [...fieldNames, fieldName];
+    const updatedInputValues = [...inputValues, inputValue];
+
+    setSelectedOptions({
+      fieldNames: updatedFieldNames,
+      inputValues: updatedInputValues,
+    });
+
+    setInputValue("");
+    inputRef.current.focus();
+  };
+
+  const removeOption = (fieldName, inputValue) => {
+    const { fieldNames, inputValues } = selectedOptions;
+    const updatedFieldNames = fieldNames.filter((name) => name !== fieldName);
+    const updatedInputValues = inputValues.filter(
+      (value) => value !== inputValue
+    );
+
+    setSelectedOptions({
+      fieldNames: updatedFieldNames,
+      inputValues: updatedInputValues,
+    });
+
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    const { fieldNames, inputValues } = selectedOptions;
+    console.log("Selected Field Names:", fieldNames);
+    console.log("Selected Input Values:", inputValues);
+    console.log("Selected Input Values:", selectedOptions);
+  }, [selectedOptions]);
+
+  const fetchData = (inputValue) => {
+    // const urls = selectedOptions.fieldNames.map((fieldName, index) => {
+    //   return `http://localhost:3000/employees?${fieldName}_like=${selectedOptions.searchValue[index]}`;
+    // });
+
     fetch("http://localhost:3000/employees")
       .then((response) => response.json())
       .then((json) => {
         const results = json.filter((employee) => {
           return (
-            value &&
+            inputValue &&
             employee &&
             employee.name &&
-            employee.name.toLowerCase().includes(value.toLowerCase())
+            employee.name.toLowerCase().includes(inputValue)
           );
         });
 
         if (results.length > 0) {
           const fieldNames = Object.keys(results[0]);
-          setFieldsNames(fieldNames);
+          console.log(fieldNames);
+          setFieldNames(fieldNames);
         }
-
-        setResults(results);
-        console.log(results);
       });
   };
 
-  const handleChange = (value) => {
-    setInput(value);
-    fetchData(value);
-    onInputChange(value);
-  };
-
   return (
-    <input
-      placeholder="Tìm..."
-      value={input}
-      onChange={(e) => handleChange(e.target.value)}
-    ></input>
+    <>
+      <div style={{ display: "flex", width: "600px" }}>
+        <div className="tag-list" id="myTagList">
+          {selectedOptions.fieldNames.map((selectedOption, index) => (
+            <button
+              className="tag-item"
+              key={selectedOption}
+              onClick={() =>
+                removeOption(selectedOption, selectedOptions.inputValues[index])
+              }
+            >
+              {selectedOption} | {selectedOptions.inputValues[index]} x
+            </button>
+          ))}
+        </div>
+        <input
+          type="text"
+          placeholder="Tìm ..."
+          value={inputValue}
+          onChange={handleChange}
+          onClick={toggleDropdown}
+          ref={inputRef}
+          id="myInput"
+          style={{ flex: 1 }}
+        />
+      </div>
+
+      <ul
+        class="dropdown"
+        id="dropdownMenu"
+        style={{ display: "none", listStyleType: "none" }}
+      >
+        {fieldNames.map((fieldName) => (
+          <li
+            style={{ textDecoration: "none" }}
+            onClick={() => addOption(fieldName, inputValue)}
+          >
+            Tìm kiếm <strong>{fieldName}</strong> cho "{inputValue}"
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
